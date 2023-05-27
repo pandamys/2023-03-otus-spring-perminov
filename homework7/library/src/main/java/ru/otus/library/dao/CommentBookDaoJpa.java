7292c27351f959ea91f64d018aff34201103148e
Vitalii Perminov <gruver2007@ya.ru>
2023-05-27 18:43:33 +0500

    init-homework7

---
+++ homework7/library/src/main/java/ru/otus/library/dao/CommentBookDaoJpa.java
@@ -0,0 +1,69 @@
+package ru.otus.library.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityGraph;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+import ru.otus.library.domain.Book;
+import ru.otus.library.domain.CommentBook;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class CommentBookDaoJpa implements CommentBookDao {
+    @PersistenceContext
+    private EntityManager em;
+
+    public CommentBookDaoJpa(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public CommentBook getById(long id) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("javax.persistence.fetchgraph", getEntityGraph());
+        return em.find(CommentBook.class, id, properties);
+    }
+
+    @Override
+    public List<CommentBook> getAll(Book book) {
+        TypedQuery<CommentBook> query;
+        query = em.createQuery("select cb from CommentBook cb where cb.book = :id", CommentBook.class);
+        query.setParameter("id", book);
+        query.setHint("javax.persistence.fetchgraph", getEntityGraph());
+        return query.getResultList();
+    }
+
+    @Override
+    public List<CommentBook> getAll(){
+        TypedQuery<CommentBook> query;
+        query = em.createQuery("select cb from CommentBook cb", CommentBook.class);
+        query.setHint("javax.persistence.fetchgraph", getEntityGraph());
+        return query.getResultList();
+    }
+
+    @Override
+    public CommentBook save(CommentBook comment) {
+        if (comment.getId() <= 0){
+            em.persist(comment);
+            return comment;
+        } else {
+            return em.merge(comment);
+        }
+    }
+
+    @Override
+    public void remove(CommentBook comment) {
+        if (em.contains(comment)){
+            em.remove(comment);
+        }
+    }
+
+    private EntityGraph<?> getEntityGraph() {
+        EntityGraph<?> entityGraph;
+        entityGraph = em.getEntityGraph("lib-comment-book-eg");
+        return entityGraph;
+    }
+}
---
+++ homework7/library/src/main/java/ru/otus/library/service/CommentServiceImpl.java
@@ -0,0 +1,85 @@
+package ru.otus.library.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.library.dao.BookDao;
+import ru.otus.library.dao.CommentBookDao;
+import ru.otus.library.domain.Book;
+import ru.otus.library.domain.CommentBook;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    private final CommentBookDao commentBookDao;
+
+    private final BookDao bookDao;
+
+    public CommentServiceImpl(CommentBookDao commentBookDao,
+                              BookDao bookDao) {
+        this.commentBookDao = commentBookDao;
+        this.bookDao = bookDao;
+    }
+
+    public CommentBook getById(long id) {
+        return commentBookDao.getById(id);
+    }
+
+    public List<CommentBook> getCommentsForBook(long bookId) {
+        Book book;
+        book = bookDao.getById(bookId);
+        if (book == null) {
+            return Collections.emptyList();
+        }
+        return commentBookDao.getAll(book);
+    }
+
+    public List<CommentBook> getAll() {
+        return commentBookDao.getAll();
+    }
+
+    @Override
+    @Transactional
+    public boolean addComment(String text, long bookId) {
+        Book book;
+        CommentBook commentBook;
+        book = bookDao.getById(bookId);
+        if (book != null){
+            commentBook = new CommentBook(text, book);
+            commentBookDao.save(commentBook);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public void updateComment(long id, String text, long bookId) {
+        CommentBook commentBook;
+        Book book;
+        commentBook = commentBookDao.getById(id);
+        if (text != null && !text.equals("")) {
+                commentBook.setText(text);
+        }
+        if (bookId > 0) {
+            book = bookDao.getById(bookId);
+            if (book != null) {
+                commentBook.setBook(book);
+            }
+        }
+        commentBookDao.save(commentBook);
+    }
+
+    @Override
+    @Transactional
+    public boolean removeComment(long id) {
+        CommentBook commentBook;
+        commentBook = commentBookDao.getById(id);
+        if (commentBook == null){
+            return false;
+        }
+        commentBookDao.remove(commentBook);
+        return true;
+    }
+}
